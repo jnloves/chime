65c5d3fa3bdf38b4bdde913d2ccc51d7615ce7fd
jnloves <jordannloves@gmail.com>
2023-10-11 15:09:39 -0400

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
+//import dynamic from 'next/dynamic'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const dynamic = "force-dynamic"
 
 export const metadata: Metadata = {
   title: 'Create Next App',
